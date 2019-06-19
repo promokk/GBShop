8731e21baee9c786f3ceaf961512124e115f9f8e
Andrey Vorobyv <andrey1803vorobvyv@yandex.ru>
2019-06-19 20:21:24 +0300

    Сделал структуру проекта

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Andrey Vorobyv on 17.06.2019.
+//  Copyright © 2019 Andrey Vorobyv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
